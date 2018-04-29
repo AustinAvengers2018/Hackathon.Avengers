1e3a26f93858fa9ae80ea0f13aea15e8a698400a
Nathan Southerland <nates1973@yahoo.com>
2018-04-29 13:18:48 -0500

    Initial check-in of models and controllers.

---
+++ Hackathon.Avengers/Avengers.Api/Models/Provider.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Avengers.Api.Models
+{
+    public class Provider
+    {
+        public int NPI { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string Specialty { get; set; }
+        public int ZIPCode { get; set; }
+        public string State { get; set; }
+        public int TotalClaimCount { get; set; }
+        public int OpiodClaimCount { get; set; }
+        public float OpioidPrescribingRate { get; set; }
+        public int ExtendedReleaseOpiodClaimCount { get; set; }
+        public float ExtendedReleaseOpiodPrescribingRate { get; set; }
+
+    }
+}
