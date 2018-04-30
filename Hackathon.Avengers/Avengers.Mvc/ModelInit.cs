9538044d557e614d28766d37cf84869746b36dda
luis.vargas <lvargas@indra.com>
2018-04-29 21:24:25 -0500

    Load Excel Provider Data from Data Directory

---
+++ Hackathon.Avengers/Avengers.Mvc/ModelInit.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Hosting;
+using Excel = Microsoft.Office.Interop.Excel;
+using Avengers.Mvc.Models;
+using System.Runtime.InteropServices;
+
+namespace Avengers.Mvc
+{
+    public static class ModelInit
+    {
+
+        public static void LoadData()
+        {
+            var Providers = LoadExcel();
+        }
+
+        public static void  LoadAzureTables( List<Provider> providers)
+        {
+
+        }
+
+
+        public static List<Provider> LoadExcel()
+        {
+            Excel.Application excel;
+            Excel.Workbook workbook;
+            Excel.Worksheet worksheet;
+
+
+            string path = HostingEnvironment.ApplicationPhysicalPath;
+            string dataPath = path + "\\Data\\Medicare_Part_D_Opioid_Prescriber_Summary_File_2016.csv";
+
+            excel = new Excel.Application();
+            workbook = excel.Workbooks.Open(dataPath);
+            worksheet = excel.ActiveSheet;
+            var rowcount = worksheet.Rows.Count;
+
+            var firstRow = 2;
+            var lastRow = rowcount;
+            var batchSize = 5000;
+            var batches = Enumerable
+                .Range(0, (int)Math.Ceiling((lastRow - firstRow) / (double)batchSize))
+                .Select(x =>
+                    string.Format(
+                        "A{0}:K{1}",
+                        x * batchSize + firstRow,
+                        Math.Min((x + 1) * batchSize + firstRow - 1, lastRow)))
+                .Select(range => ((Array)worksheet.Range[range].Cells.Value));
+
+
+            var providers = new List<Provider>();
+
+
+            foreach (var batch in batches)
+            {
+                foreach (var item in batch)
+                {
+                    if (item != null)
+                    {
+                    var srow = item;
+                    String[] rowitems = srow.ToString().Split(new char[] {';'});
+
+                    var provider = new Provider(rowitems);
+                    providers.Add(provider);
+                    }
+                }
+            }
+
+            //cleanup
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+            Marshal.ReleaseComObject(worksheet);
+            workbook.Close();
+            Marshal.ReleaseComObject(workbook);
+            excel.Quit();
+            Marshal.ReleaseComObject(excel);
+
+            return providers;
+
+        }
+    }
+}
---
+++ Hackathon.Avengers/Avengers.Mvc/Models/Provider.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Avengers.Mvc.Models
+{
+    public class Provider
+    {
+        public long ProviderID { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string ZipCode { get; set; }
+        public string State { get; set; }
+        public string Specialty { get; set; }
+        public int Total { get; set; }
+        public int  Opioid { get; set; }
+        public string OpioidRateS { get; set; }
+        public decimal OpioidRateD { get; set; }
+        public int ExtendedOpioid { get; set; }
+        public string ExtendedOpioidRateS { get; set; }
+        public decimal ExtendedOpioidRateD { get; set; }
+
+        public Provider(String[] items)
+        {
+            long ProviderIDL;
+            if (items.Length >= 9 && items.Length < 12 &&  items[0] != "" && long.TryParse(items[0], out ProviderIDL))
+            {
+                ProviderID = Convert.ToInt64(items[0]);
+                LastName = items[1];
+                FirstName = items[2];
+                ZipCode = items[3];
+                State = items[4];
+                Specialty = items[5];
+                Total = Convert.ToInt32(items[6]);
+                if (items.Length > 7 && items[7] != "")
+                {
+                    Opioid = Convert.ToInt32(items[7]);
+                }
+                if (items.Length > 8 && items[8] != "" )
+                {
+                    OpioidRateS = items[8];
+                    OpioidRateD = decimal.Parse(OpioidRateS.TrimEnd(new char[] { '%', ' ' }));
+                }
+
+                if (items.Length > 9 && items[9] != "" ) 
+                {
+                    ExtendedOpioid = Convert.ToInt32(items[9]);
+                }
+                if (items.Length > 10 && items[10] != "")
+                {
+                    ExtendedOpioidRateS = items[10];
+                    ExtendedOpioidRateD = decimal.Parse(ExtendedOpioidRateS.TrimEnd(new char[] { '%', ' ' }));
+                }
+            }
+
+        }
+    }
+}
