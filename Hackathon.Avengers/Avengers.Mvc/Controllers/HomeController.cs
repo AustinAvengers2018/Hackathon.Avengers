86e061e9fb5b4f848a5ad4e8f0ee437d75a2ecab
Rachel Frock <rachel.frock@CUC.local>
2018-05-04 10:36:37 -0500

    re cloned project and added basic routing template- still needs cleanup

--- Hackathon.Avengers/Avengers.Mvc/Controllers/HomeController.cs
+++ Hackathon.Avengers/Avengers.Mvc/Controllers/HomeController.cs
@@ -26,5 +26,40 @@ namespace Avengers.Mvc.Controllers
 
             return View();
         }
+
+        public ActionResult Login()
+        {
+            ViewBag.Message = "Your login page.";
+
+            return View();
+        }
+
+        public ActionResult AdminLogin()
+        {
+            ViewBag.Message = "Your admin login page.";
+
+            return View();
+        }
+
+        public ActionResult Dashboard()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Report()
+        {
+            ViewBag.Message = "Your report page.";
+
+            return View();
+        }
+
+        public ActionResult ActivityDetail()
+        {
+            ViewBag.Message = "Your activity detail page.";
+
+            return View();
+        }
     }
 }
