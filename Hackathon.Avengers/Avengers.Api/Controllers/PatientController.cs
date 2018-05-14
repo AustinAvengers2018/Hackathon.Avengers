70fec3177f3f59fcbf4fb8a9c683be47140c37dd
Nathan Southerland <nates1973@yahoo.com>
2018-05-14 18:55:27 -0500

    Rename Prescription controller to Patient; Flesh out missing method bodies.

---
+++ Hackathon.Avengers/Avengers.Api/Controllers/PatientController.cs
@@ -0,0 +1,52 @@
+﻿using Avengers.Api.DataAccess;
+using Avengers.Api.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Avengers.Api.Controllers
+{
+    public class PatientController : ApiController
+    {
+
+        readonly FraudStorageRepository _repo;
+
+        public PatientController(IAvengersCloudAccess cloud)
+        {
+            _repo = new FraudStorageRepository(cloud);
+        }
+
+        // GET: api/Prescription
+        public IEnumerable<PatientEntity> Get()
+        {
+            return _repo.Patients.Get();
+        }
+
+        // GET: api/Prescription/5
+        public PatientEntity Get(string rxNumber)
+        {
+            return _repo.Patients.Find(rxNumber);
+        }
+
+        // POST: api/Prescription
+        public void Post([FromBody]PatientEntity rx)
+        {
+            _repo.Patients.Create(rx);
+        }
+
+        // PUT: api/Prescription/5
+        public void Put(string rxNumber, [FromBody]PatientEntity rx)
+        {
+            _repo.Patients.Update(rxNumber, rx);
+        }
+
+        // DELETE: api/Prescription/5
+        public void Delete(string id)
+        {
+            _repo.Patients.Delete(id);
+        }
+    }
+}
