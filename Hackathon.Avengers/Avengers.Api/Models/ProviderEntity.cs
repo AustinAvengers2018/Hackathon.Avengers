badec70aa8bf28d4c2d83e61eeefb672430863dc
Nathan Southerland <nates1973@yahoo.com>
2018-05-15 18:24:19 -0500

    Improve mapping of Azure entity to local entity

--- Hackathon.Avengers/Avengers.Api/Models/ProviderEntity.cs
+++ Hackathon.Avengers/Avengers.Api/Models/ProviderEntity.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using Microsoft.WindowsAzure.Storage;
 
 namespace Avengers.Api.Models
 {
@@ -16,7 +17,7 @@ namespace Avengers.Api.Models
         public long National99Percentile { get; set; }
         public long NationalRank { get; set; }
         public int Opiod { get; set; }
-        public long ProviderID { get; set; }
+        public string ProviderID { get; set; }
         public bool Reviewed { get; set; }
         public string Specialty { get; set; }
         public int ExtendedOpiod { get; set; }
@@ -26,5 +27,26 @@ namespace Avengers.Api.Models
         public int Total { get; set; }
         public string ZipCode { get; set; }
 
+
+        public override void ReadEntity(IDictionary<string, EntityProperty> properties, OperationContext operationContext)
+        {
+            PartitionKey = properties["PartitionKey"].StringValue;
+            RowKey = properties["RowKey"].StringValue;
+            Timestamp = properties["Timestamp"].DateTime ?? DateTime.Today.AddYears(-50);
+            FirstName = properties["FirstName"].StringValue;
+            LastName = properties["LastName"].StringValue;
+            National99Percentile = properties["National99Percentile"].Int64Value ?? Int64.MinValue;
+            NationalRank = properties["NationalRank"].Int32Value ?? Int64.MinValue;
+            Opiod = properties["Opiod"].Int32Value ?? Int32.MinValue;
+            ProviderID = properties["ProviderID"].StringValue;
+            Reviewed = properties["Reviewed"].BooleanValue ?? false;
+            Specialty = properties["Specialty"].StringValue;
+            ExtendedOpiod = properties["ExtendedOpiod"].Int32Value ?? Int32.MinValue;
+            State = properties["State"].StringValue;
+            State99Percentile = properties["State99Percentile"].Int64Value ?? Int64.MinValue;
+            StateRank = properties["StateRank"].Int32Value ?? Int32.MinValue;
+            Total = properties["Total"].Int32Value ?? Int32.MinValue;
+            ZipCode = properties["ZipCode"].StringValue;
+        }
     }
 }
